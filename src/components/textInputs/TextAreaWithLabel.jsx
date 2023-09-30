b8c05dfc08fd1f2726b140c54c6cf4d3cded71b3
prizkideanne <prizkideanne@gmail.com>
2023-10-01 00:47:50 +0700

    fix multerarray

--- src/components/textInputs/TextAreaWithLabel.jsx
+++ src/components/textInputs/TextAreaWithLabel.jsx
@@ -4,11 +4,11 @@ function TextAreaWithLabel({ field, label, ...props }) {
   const { errors } = props?.form;
 
   return (
-    <div className="space-y-1">
+    <div className="space-y-1 flex flex-col">
       <label className="text-sm capitalize ">{label}</label>
       <textarea
         rows={4}
-        className="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+        className="block w-full rounded-md border py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
         {...field}
         {...props}
       />
